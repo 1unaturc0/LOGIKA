9c92c208dd4297669ddadc7edf29e91a459cb050
Alexander <xela.vomiza@gmail.com>
2024-09-13 00:56:51 +0300

    Confirm row button tweak

--- src/components/game/board/cells_row/confirm_row_button/ConfirmRowButton.tsx
+++ src/components/game/board/cells_row/confirm_row_button/ConfirmRowButton.tsx
@@ -6,17 +6,11 @@ import { RootState } from "#/redux/store";
 import styles from "./ConfirmRowButton.module.css";
 
 const ConfirmRowButton = () => {
-	const areEmptyCells = useSelector((state: RootState) => state.settings.areEmptyCells);
-	const { cells, activeCell } = useSelector((state: RootState) => state.game);
-	const enabled = areEmptyCells || !cells[activeCell.row].includes(0);
+	const { activeCell } = useSelector((state: RootState) => state.game);
 	const { confirmRow } = useActions();
 
-	const onClick = () => {
-		if (enabled) confirmRow();
-	};
-
 	useEffect(() => {
-		if (activeCell.column !== null || !enabled) return;
+		if (activeCell.column !== null) return;
 
 		const onKeyDown = (e: KeyboardEvent) => {
 			if (e.key === "Enter" || e.key === "w" || e.key === "ArrowUp") confirmRow();
@@ -24,14 +18,13 @@ const ConfirmRowButton = () => {
 
 		document.addEventListener("keydown", onKeyDown);
 		return () => document.removeEventListener("keydown", onKeyDown);
-	}, [confirmRow, activeCell.column, enabled]);
+	}, [confirmRow, activeCell.column]);
 
 	return (
 		<button
-			onClick={onClick}
+			onClick={() => confirmRow()}
 			className={`
         ${styles.confirmRowBtn} \
-        ${enabled || styles.disabled} \
         ${activeCell.column === null && styles.active}
       `}
 		>
